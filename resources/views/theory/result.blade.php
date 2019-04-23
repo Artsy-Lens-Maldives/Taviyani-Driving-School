59c33cacf45ffe488800466939ff23397149fa79
athik13 <athik.13@gmail.com>
2019-04-23 20:37:58 +0800

    vbn

--- resources/views/theory/result.blade.php
+++ resources/views/theory/result.blade.php
@@ -3,13 +3,48 @@
 @section('content')
 
 <div class="container">
-    @foreach ($newArray as $item)
-        <?php print_r($item) ?>
-    @endforeach
+    <div class="card" style="margin-top: 100px;">
+        <div class="card-body">
+            <h1>Theory Paper: Result</h1>
+            <hr>
+            <div class="score">
+                <span class="score-text">Final Score</span>
+                <br>
+                <span class="score-percent">{{ $result[0]['percent'] }}
+                </span>
+                <br>
+                <span class="score-text">{{ $result[0]['correct'] }} of {{ $result[0]['total'] }} correct</span>
+            </div>
+            <hr>
 
-    <hr>
-
-    <?php print_r($result) ?>
+            @foreach ($newArray as $item)
+                {{ $item[0]['question'] }}
+            @endforeach
+        </div>
+    </div>
 </div>
+
+@endsection
+
+@section('css')
+
+<style>
+    body {
+        background-color: #f2f8ff;
+        background-image: url('/logo/dr-r.png');
+        background-repeat: repeat;
+    }
+    .score {
+        text-align: center;
+    }
+    .score-text {
+        font-size: 60px;
+        color: blue;
+    }
+    .score-percent {
+        font-size: 55px;
+        color: red;
+    }
+</style>
 
 @endsection
